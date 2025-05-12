c055e1e78236057f6a433c37a44ee0fd49108f77
GentleHoney <l.barletta24@gmail.com>
2025-05-13 00:53:33 +0200

    minor changes

--- skaniscript.sh
+++ skaniscript.sh
@@ -1,2 +1,3 @@
-skani dist -r "/home/lorenzo/Documents/tesi_2/skani-time-depency/reference_0.fasta" --ql "/home/lorenzo/Documents/tesi_2/fastANI-skani-comparison/skani-time-depency/skani_genome_paths/paths_100.txt" -o "/home/lorenzo/Documents/tesi_2/fastANI-time-depency/times_output/skanitime.txt" > /dev/null 2>&1
-
+for i in $(seq 1 1000); do
+    skani dist -r "/home/lorenzo/Documents/tesi_2/fastANI-skani-comparison/skani-time-depency/reference_0.fasta" --ql "/home/lorenzo/Documents/tesi_2/fastANI-skani-comparison/skani-time-depency/skani_genome_paths/paths_${i}.txt"  > /dev/null 2>&1
+done 
