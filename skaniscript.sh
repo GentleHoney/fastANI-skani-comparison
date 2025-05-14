5cf1a2ca903160508f4425d591537c5875789345
GentleHoney <l.barletta24@gmail.com>
2025-05-15 01:48:57 +0200

    N pairwise time dependency

--- skaniscript.sh
+++ skaniscript.sh
@@ -1,3 +1,3 @@
-for i in $(seq 1 1000); do
-    skani dist -r "/home/lorenzo/Documents/tesi_2/fastANI-skani-comparison/skani-time-depency/reference_0.fasta" --ql "/home/lorenzo/Documents/tesi_2/fastANI-skani-comparison/skani-time-depency/skani_genome_paths/paths_${i}.txt"  > /dev/null 2>&1
+for i in $(seq 1 100); do
+    skani dist -r "/home/lorenzo/Documents/tesi_2/fastANI-skani-comparison/skani-time-depency/reference_0.fasta" --ql "/home/lorenzo/Documents/tesi_2/fastANI-skani-comparison/skani-time-depency/skani_genome_paths/paths_${i}.txt" -o "/home/lorenzo/Documents/tesi_2/fastANI-skani-comparison/skani-time-depency/skani_times_output/results${i}.txt"  
 done 
